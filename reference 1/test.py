92c0b467f8d57955c3e32152dfcfafd39cd72b4d
appeventuremoment <h2010027@nushigh.edu.sg>
2024-04-09 19:05:38 +0800

    Touched up + Attempted to run model, switching to another device

--- reference 1/test.py
+++ reference 1/test.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import cv2
+import os
 from keras.models import load_model
 import numpy as np
 from keras import *
@@ -41,15 +42,12 @@ transfer_mobile_model.compile(optimizer=tf.optimizers.Adam(),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-transferhistory = transfer_mobile_model.fit(train, validation_data=val, epochs=10)
+transferhistory = transfer_mobile_model.fit(train, validation_data=val, epochs=5)
 
-
-
-# model = load_model('reference 1/keras_model.h5')
-model = transfer_mobile_model
-
-   
 count = 0
+labels = next(os.walk("images/train"))[1]
+detected = None
+
 while True:
 	success, imgOrignal=cap.read()
 	faces = facedetect.detectMultiScale(imgOrignal,1.3,5)
@@ -57,34 +55,32 @@ while True:
 		crop_img=imgOrignal[y:y+h,x:x+h]
 		img=cv2.resize(crop_img, (224,224))
 		img=img.reshape(1, 224, 224, 3)
-		prediction=model.predict(img)
+		prediction=transfer_mobile_model.predict(img)
 		classIndex = np.argmax(prediction)
 
-		if classIndex==0:
-			cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(0,255,0),2)
-			cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (0,255,0),-2)
-			cv2.putText(imgOrignal, 'Aik Lok',(x,y-10), font, 0.75, (255,255,255),1, cv2.LINE_AA)
+		cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(0,255,0),2)
+		cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (0,255,0),-2)
+		cv2.putText(imgOrignal, labels[classIndex],(x,y-10), cv2.FONT_HERSHEY_COMPLEX, 0.75, (255,255,255),1, cv2.LINE_AA)
+
+		if labels[classIndex] == 'not person':
+			continue
+
+		if labels[classIndex] == detected:
 			count += 1
-			if count > 200000000: # TODO: CHANGE TO 20
-				count = 0
-				print('index 0')
-				import sys
-				sys.exit()
-		elif classIndex==1:
-			cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(0,255,0),2)
-			cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (0,255,0),-2)
-			cv2.putText(imgOrignal, 'idk',(x,y-10), font, 0.75, (255,255,255),1, cv2.LINE_AA)
 		else:
-			cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(0,255,0),2)
-			cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (0,255,0),-2)
-			cv2.putText(imgOrignal, 'yew',(x,y-10), font, 0.75, (255,255,255),1, cv2.LINE_AA)
+			detected = labels[classIndex]
+
+		if count > 20:
+			count = 0
+			print(f"{labels[classIndex]} detected. Enter")
+			import sys
+			sys.exit()
 			
 
 	cv2.imshow("Result",imgOrignal)
 	k=cv2.waitKey(1)
 	if k==ord('q'):
 		break
-
 
 cap.release()
 cv2.destroyAllWindows()
